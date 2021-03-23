3d806f75875d750c10a4115a8d4ec5dfd78e130e
Benjamin Bruemmer <benjamin_bruemmer@clinicalarchitecture.com>
2021-03-23 09:56:32 -0400

    Initial commint

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            //return "lot of products";
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> GetProduct(int id)
+        {
+            //return "one product";
+            var product = await _context.Products.FindAsync(id);
+            return Ok(product);
+
+        }
+    }
+}
