bf029aab5b2c4113618394fbcd6717570c499f3f
Benjamin Bruemmer <benjamin_bruemmer@clinicalarchitecture.com>
2021-04-09 09:40:23 -0400

    End of section 8

---
+++ client/src/demo.ts
@@ -0,0 +1,27 @@
+// this means data can be a number or a string
+let data: number | string;
+
+data = '42';
+
+data = 10;
+
+interface ICar {
+    color: string;
+    model: string;
+    topSpeed?: number;
+}
+
+const car1: ICar = {
+    color: 'blue',
+    model: 'bmw'
+};
+
+const car2: ICar = {
+    color: 'red',
+    model: 'mercedes',
+    topSpeed: 100
+};
+
+const multiply = (x: number, y: number): string => {
+    return (x * y).toString();
+};
